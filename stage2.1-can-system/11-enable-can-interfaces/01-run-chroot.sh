5f092a67bf0271b543a5c6eab0bda7cdf36a0f6a
Tobias Jaehnel <tjaehnel@gmail.com>
2021-03-20 23:08:57 +0100

    Automatically restart can when bus goes off.

--- stage2.1-can-system/11-enable-can-interfaces/01-run-chroot.sh
+++ stage2.1-can-system/11-enable-can-interfaces/01-run-chroot.sh
@@ -12,7 +12,7 @@ echo ""
 sudo bash -c 'cat << EOF > /etc/network/interfaces.d/can0
 auto can0
 iface can0 inet manual
-   pre-up /sbin/ip link set can0 type can bitrate 500000 triple-sampling on
+   pre-up /sbin/ip link set can0 type can restart-ms 100 bitrate 500000 triple-sampling on
    up /sbin/ifconfig can0 up
    down /sbin/ifconfig can0 down
 EOF'
@@ -20,7 +20,7 @@ EOF'
 sudo bash -c 'cat << EOF > /etc/network/interfaces.d/can1
 auto can1
 iface can1 inet manual
-    pre-up /sbin/ip link set can1 type can bitrate 500000 triple-sampling on
+    pre-up /sbin/ip link set can1 type can restart-ms 100 bitrate 500000 triple-sampling on
     up /sbin/ifconfig can1 up
     down /sbin/ifconfig can1 down
 EOF'
