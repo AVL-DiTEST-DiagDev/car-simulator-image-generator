bc46c941435a43d5555b740f28960bad273181be
Tobias Jaehnel <tjaehnel@gmail.com>
2022-12-15 16:12:50 +0100

    Build libdoip

---
+++ stage2.2-carsimulator/01-build-carsim/03-run-chroot.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+# Author :  Julia Fuchs   (julia.fuchs@avl.com)
+#           Emil Brunner  (emil.brunner@avl.com)
+#           Tobias Jaehnel (tjaehnel@gmail.com)
+# 
+echo "______________________________________________________________________________"
+echo ""
+echo "			Compile and install car-simulator (AVL DiTEST GmbH)"
+echo ""
+
+# clone car-simulator
+cd /opt
+rm -rf car-simulator
+git clone -q https://github.com/AVL-DiTEST-DiagDev/car-simulator.git
+cd car-simulator
+
+# PATCH - fix path to libcrc/crcccitt.c in ecu_lua_script.cpp
+cat<<EOF | patch --directory=src
+*** ecu_lua_script.cpp  2019-05-02 02:22:45.081810699 +0100
+--- ecu_lua_script.cpp  2019-05-02 02:25:16.741270323 +0100
+***************
+*** 5,11 ****
+   */
+
+  #include "ecu_lua_script.h"
+! #include "crcccitt.c"
+  #include "utilities.h"
+  #include <iostream>
+  #include <string.h>
+--- 5,11 ----
+   */
+
+  #include "ecu_lua_script.h"
+! #include "libcrc/crcccitt.c"
+  #include "utilities.h"
+  #include <iostream>
+  #include <string.h>
+EOF
+
+
+# make car-simulator
+make
